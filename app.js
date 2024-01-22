1d8df4b4ee42eaa0def00aa8f892d1e0c8170a23
Mitiomgs <157385340+Mitiomgs@users.noreply.github.com>
2024-01-22 11:33:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');}
+    }
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
+}
